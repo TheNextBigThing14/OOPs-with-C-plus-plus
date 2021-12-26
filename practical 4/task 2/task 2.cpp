0e062e413cda04153988eec823ccbfb52c85f5f2
mirinal kapri <88933275+TheNextBigThing14@users.noreply.github.com>
2021-12-27 01:15:21 +0530

    Create task 2.cpp

---
+++ practical 4/task 2/task 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+ using namespace std;
+ int main() {
+ int a,b;
+ cout<<"enter the values of a and b :";
+ cin>>a>>b;
+ cout<<a+b<<endl;
+ float c,d;
+ cout<<"enter the values of c and d:";
+ cin>>c>>d;
+ cout<<c+d<<endl;
+ return 0;
+}
