c8e25104b3497272641c8930eff3f218c67f3955
mirinal kapri <88933275+TheNextBigThing14@users.noreply.github.com>
2021-12-27 01:18:06 +0530

    Create task 1.cpp

---
+++ practical 5/task 1/task 1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+int no=45;
+int salary=85000;
+
+float rate=98.34;
+double money=46E12;
+
+char c='h';
+wchar_t test1=L'D';
+
+bool cond=true;
+cout<<"Int :"<< no <<endl;
+cout<<"Int:"<< salary <<endl;
+cout<<"Float:"<< rate <<endl;
+cout<<"Double : "<< money <<endl;
+cout<<"Char :"<< c <<endl;
+cout<<"Wchar : "<< test1 <<endl;
+cout<<"Booleab: "<< cond <<endl;
+return 0;
+}
