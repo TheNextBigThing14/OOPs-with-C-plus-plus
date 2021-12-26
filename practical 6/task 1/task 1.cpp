f404e8e967d0d17a6ecd0caca6bb549398d420bd
mirinal kapri <88933275+TheNextBigThing14@users.noreply.github.com>
2021-12-27 01:21:11 +0530

    Create task 1.cpp

---
+++ practical 6/task 1/task 1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=5;
+	string s1="ABC";
+	string s2=a+s1;
+	cout<<s2<<endl;
+	return 1;
+}
+
+#include<iostream>
+using namespace std;
+
+int main( )
+{
+    char str[80];
+
+    cout<<"Enter string: ";
+    cin.getline(str, 80);
+	
+    int l,i; 
+    
+    
+    for(l = 0; str[l] != '\0'; l++);
+
+  
+    for(i = 0; (i < l/2) && (str[i] == str[l - i - 1]); i++);
+
+    if(i == l/2)
+        cout << "Palindrome";
+    else
+        cout << "Not a palindrome";
+	
+    return 0;
+}
